858933ab900715a74eb32482d246c287891e4c00
superiorkid <bayiPetani@protonmail.com>
2024-04-28 12:36:35 +0800

    feat: implement route to get post by slug with existence checks
    * Added GET route to retrieve post details by slug
    * Implemented check for post existence before returning data
    * Throws a "Not Found" exception if the post with the provided slug doesn't exist
    * Improved data retrieval logic for successful post retrieval

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,10 +1,23 @@
-import { Body, Controller, Post, Req, ValidationPipe } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpStatus,
+  InternalServerErrorException,
+  NotFoundException,
+  Param,
+  Post,
+  Req,
+  ValidationPipe,
+} from '@nestjs/common';
 import {
   ApiBearerAuth,
   ApiConflictResponse,
   ApiConsumes,
   ApiCreatedResponse,
   ApiInternalServerErrorResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
   ApiTags,
 } from '@nestjs/swagger';
 import { FormDataRequest } from 'nestjs-form-data';
@@ -34,5 +47,42 @@ export class PostsController {
     @Body(new ValidationPipe()) createPostDto: CreatePostDto,
   ) {
     return this.postsService.create({ createPostDto, userId: req.user.sub });
+  }
+
+  /**
+   * Endpoint for retrieving a post by its slug.
+   * @param slug - The slug of the post to retrieve.
+   * @returns A response containing the retrieved post.
+   */
+  @ApiTags('Posts')
+  @ApiOkResponse({ description: 'Get post successfully' })
+  @ApiNotFoundResponse({ description: 'Post not found' })
+  @ApiInternalServerErrorResponse({ description: 'Internal server error' })
+  @Get(':slug')
+  async getPost(@Param('slug') slug: string) {
+    try {
+      // Retrieve the post from the service based on the provided slug
+      const post = await this.postsService.findOne({ slug });
+      // If the post is not found, throw a NotFoundExceptio
+      if (!post) throw new NotFoundException('post not found');
+
+      // Return a response indicating successful post retrieval
+      return {
+        message: 'Get post successfully',
+        statusCode: HttpStatus.OK,
+        data: post,
+      };
+    } catch (error) {
+      // If the error is a NotFoundException, re-throw it
+      if (error instanceof NotFoundException) {
+        throw error;
+      } else {
+        // Log the error and throw an InternalServerErrorException
+        console.error(error);
+        throw new InternalServerErrorException(
+          'something went wrong. failed to get post.',
+        );
+      }
+    }
   }
 }
