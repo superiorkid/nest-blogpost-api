ae4f91dd1faa8ab430d76fd2b6d8a950fddfbc9c
superiorkid <bayiPetani@protonmail.com>
2024-04-30 07:40:27 +0800

    feat: integrate Nest Static module for serving post cover images
    * Installed `@nestjs/serve-static` package for static file serving
    * Imported `ServeStaticModule` into `AppModule.ts`
    * Configured `ServeStaticModule` to serve post cover images from a designated directory
    * Injected the static file serving functionality using the `forRoot` method

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { NestjsFormDataModule } from 'nestjs-form-data';
+import { join } from 'path';
 import { AuthenticationModule } from './authentication/authentication.module';
 import { DatabasesModule } from './databases/databases.module';
-import { UsersModule } from './users/users.module';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 import { PostsModule } from './posts/posts.module';
-import { NestjsFormDataModule } from 'nestjs-form-data';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
+    // ServeStaticModule for serving static files
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'), // Path to the public directory
+      exclude: ['/api/(.*)'], // Exclude URLs matching the specified pattern from static file serving
+    }),
+    // ConfigModule for loading configuration, made global to be available across the app
     ConfigModule.forRoot({ isGlobal: true }),
+    // NestjsFormDataModule for handling form data
     NestjsFormDataModule.config({ isGlobal: true }),
     DatabasesModule,
     AuthenticationModule,
